aef965997bc8cdf418d77648f7c174bc89ef9f1e
vitor2233 <vitorguilherme2233@gmail.com>
2024-01-14 21:02:18 -0300

    Adding notifications subdomain

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,8 +1,8 @@
 import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
-import { NotAllowedError } from './errors/not-allowed-error'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository'
 import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
@@ -47,9 +47,9 @@ export class EditQuestionUseCase {
 
         questionAttachmentList.update(questionAttachments)
 
+        question.attachments = questionAttachmentList
         question.title = title
         question.content = content
-        question.attachments = questionAttachmentList
 
         await this.questionsRepository.save(question)
 
