5c8b500b51e4a7eb2982508db55d33e3c4730a95
vitor2233 <vitorguilherme2233@gmail.com>
2024-01-16 11:30:04 -0300

    Send notification on answer event

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -28,6 +29,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
     async create(answer: Answer): Promise<void> {
         this.items.push(answer)
+
+        DomainEvents.dispatchEventsForAggregate(answer.id)
     }
 
     async save(answer: Answer): Promise<void> {
